75b17c3215b31acb9750c62904d74a74de5a4a3b
pyjoker <karta1597596@gmail.com>
2020-11-01 16:15:22 +0800

    Initial Commit

---
+++ ios_data_binding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_data_binding
+//
+//  Created by mac on 2020/11/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_data_binding/ios_data_bindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_data_bindingApp.swift
+//  ios_data_binding
+//
+//  Created by mac on 2020/11/1.
+//
+
+import SwiftUI
+
+@main
+struct ios_data_bindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
